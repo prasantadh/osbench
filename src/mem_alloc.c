1123e67468ef0af4e4033d0d3ce3ea2c14eea585
prasant adhikari <pa1038@nyu.edu>
2023-02-19 10:06:36 -0500

    mem_alloc runs for 5k loops instead of 5s

--- src/mem_alloc.c
+++ src/mem_alloc.c
@@ -6,7 +6,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static const double BENCHMARK_TIME = 1200.0;
 #define NUM_ALLOCS 1000000
 
 static void* s_addresses[NUM_ALLOCS];
@@ -14,13 +13,8 @@ static void* s_addresses[NUM_ALLOCS];
 int main(int argc, const char** argv) {
   (void)argc;
   (void)argv;
-  printf("Benchmark: Allocate/free %d memory chunks (4-128 bytes)...\n", NUM_ALLOCS);
-  fflush(stdout);
 
-  double best_time = 1e9;
-  const double start_t = get_time();
-  while (get_time() - start_t < BENCHMARK_TIME) {
-    const double t0 = get_time();
+  for (int i = 0; i < 5000; ++i) {
 
     for (int i = 0; i < NUM_ALLOCS; ++i) {
       const size_t memory_size = ((i % 32) + 1) * 4;
@@ -32,14 +26,7 @@ int main(int argc, const char** argv) {
       free(s_addresses[i]);
     }
 
-    double dt = get_time() - t0;
-    if (dt < best_time) {
-      best_time = dt;
-    }
   }
-
-  printf("%f ns / alloc\n", (best_time / (double)NUM_ALLOCS) * 1000000000.0);
-  fflush(stdout);
 
   return 0;
 }
